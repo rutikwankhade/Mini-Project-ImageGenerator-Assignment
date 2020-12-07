fdf506d5de8a20b7b69095b3ed1b260b7ad9b939
rutik wankhade <rutikwankhade2@gmail.com>
2020-12-07 21:33:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from "./logo.svg";
 import "./css/App.css";
 import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+     
     </div>
   );
 }

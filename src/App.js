c2b85f7e5872cf1191ca8e19910b57262e807c58
rutik wankhade <rutikwankhade2@gmail.com>
2020-12-08 19:01:45 +0530

    feat: add api and style the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import "./css/App.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ImageDisplay from './components/ImageDisplay';
 
 function App() {
+  const [imgUrl, setImgUrl] = useState('');
+  const [loading, setLoading] = useState(false);
+
+
+  const getImage = () => {
+    setLoading(true);
+    fetch('https://picsum.photos/200/300')
+      .then(res => res.url)
+      .then(url => {
+        setLoading(false);
+        setImgUrl(url)
+        console.log(url)
+      })
+      .catch(error => console.log(error))
+
+
+  }
+
+
+  useEffect(() => {
+    getImage();
+
+  }, [])
   return (
     <div className="App">
-     
+      <button onClick={() => getImage()}>Generate new Image</button>
+      <ImageDisplay imgUrl={imgUrl} loading={loading} />
     </div>
   );
 }
